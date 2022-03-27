9223f49ea273550f3309173477e28523637b5ac0
Blackarcheruz <Breakingreport4@gmail.com>
2022-03-27 12:53:02 +0500

    new Release

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,9 +5,6 @@ const Navbar = () => {
         <div className="container">
         <nav>
           <Link to="/"><h2>NAMOZ VAQTLARI</h2></Link>
-           <ul>
-        <li><a href="https://listen-quran.cf/">Listen Quran </a></li>
-        </ul>
         </nav>    
         </div>
     )
